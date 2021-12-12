7a7bd5e7659d5e706684818163fb45472b9dbd1e
Artem <lngaart55@gmail.com>
2021-12-12 23:33:48 +0300

    Corrections

--- K_PlotFlywheels/main.cpp
+++ K_PlotFlywheels/main.cpp
@@ -4,34 +4,32 @@
 #include <vector>
 #include <numeric>
 
-template <typename T>
+template <typename T, typename U>
 class IGraph {
 public:
     using Vertex = T;
 
     struct Edge {
-        Edge(Vertex start, T old_weight, Vertex end, T new_weight)
-            : start_edge{start}, old_weight_edge{old_weight}, end_edge{end}, new_weight_edge{new_weight} {
+        Edge(Vertex start, Vertex end, U new_weight = 1) : start_edge{start}, end_edge{end}, weight{new_weight} {
         }
 
         Vertex start_edge;
-        T old_weight_edge;
-
         Vertex end_edge;
-        T new_weight_edge;
+
+        U weight;
     };
 
     virtual const std::vector<Vertex> &GetNeighbors(Vertex v) const = 0;
-    virtual void AddEdge(Vertex from, T old_weight, Vertex to, T new_weight) = 0;
+    virtual void AddEdge(Vertex from, Vertex to, U weight = 1) = 0;
     virtual const std::vector<Edge> &GetEdges() const = 0;
     virtual size_t Size() const = 0;
     virtual ~IGraph() = default;
 };
 
-template <typename T>
-class ListGraph : public IGraph<T> {
-    using typename IGraph<T>::Vertex;
-    using typename IGraph<T>::Edge;
+template <typename T, typename U>
+class ListGraph : public IGraph<T, U> {
+    using typename IGraph<T, U>::Vertex;
+    using typename IGraph<T, U>::Edge;
 
     std::vector<std::vector<Vertex>> vertices_;
     std::vector<Edge> edges_;
@@ -44,9 +42,9 @@ public:
         return vertices_[v];
     }
 
-    void AddEdge(Vertex from, T old_weight, Vertex to, T new_weight) override {
+    void AddEdge(Vertex from, Vertex to, U weight = 1) override {
         vertices_[from].emplace_back(to);
-        edges_.emplace_back(from, old_weight, to, new_weight);
+        edges_.emplace_back(from, to, weight);
     }
 
     const std::vector<Edge> &GetEdges() const override {
@@ -59,23 +57,32 @@ public:
 };
 
 template <typename T>
-void FindMinArrivalTime(IGraph<T> &teleportation_stations, std::vector<T> &arrival_times_at_station, T start_station) {
+struct EdgeWeights {
+    T old_weight_edge;
+    T new_weight_edge;
+};
+
+template <typename T>
+void FindMinArrivalTime(IGraph<T, EdgeWeights<T>> &teleportation_stations, std::vector<T> &arrival_times_at_station,
+                        T start_station) {
     arrival_times_at_station[start_station] = 0;
 
     const auto &teleportations = teleportation_stations.GetEdges();
 
     for (size_t i{0}; i < teleportations.size(); ++i) {
         for (const auto &current_teleportation : teleportations) {
-            if (arrival_times_at_station[current_teleportation.start_edge] <= current_teleportation.old_weight_edge) {
-                arrival_times_at_station[current_teleportation.end_edge] = std::min(
-                    arrival_times_at_station[current_teleportation.end_edge], current_teleportation.new_weight_edge);
+            if (arrival_times_at_station[current_teleportation.start_edge] <=
+                current_teleportation.weight.old_weight_edge) {
+                arrival_times_at_station[current_teleportation.end_edge] =
+                    std::min(arrival_times_at_station[current_teleportation.end_edge],
+                             current_teleportation.weight.new_weight_edge);
             }
         }
     }
 }
 
 template <typename T>
-void PrintMinArrivalTime(IGraph<T> &teleportation_stations, T start_station, T end_station) {
+void PrintMinArrivalTime(IGraph<T, EdgeWeights<T>> &teleportation_stations, T start_station, T end_station) {
     std::vector<T> arrival_times_at_station(teleportation_stations.Size(), std::numeric_limits<T>::max());
 
     FindMinArrivalTime(teleportation_stations, arrival_times_at_station, start_station);
@@ -90,11 +97,11 @@ void Initialization(size_t &num_stations, size_t &num_teleportations, T &start_s
 }
 
 template <typename T>
-void BuildTeleportationGraph(IGraph<T> &graph, size_t num_edges) {
+void BuildTeleportationGraph(IGraph<T, EdgeWeights<T>> &graph, size_t num_edges) {
     for (size_t i{0}; i < num_edges; ++i) {
         T from{0}, old_weight{0}, to{0}, new_weight{0};
         std::cin >> from >> old_weight >> to >> new_weight;
-        graph.AddEdge(from - 1, old_weight, to - 1, new_weight);  // We use numbering from 0
+        graph.AddEdge(from - 1, to - 1, {old_weight, new_weight});  // We use numbering from 0
     }
 }
 
@@ -108,7 +115,7 @@ int main() {
 
     Initialization(num_stations, num_teleportations, start_station, end_station);
 
-    ListGraph<int> teleportation_stations(num_stations);
+    ListGraph<int, EdgeWeights<int>> teleportation_stations(num_stations);
 
     BuildTeleportationGraph(teleportation_stations, num_teleportations);
 
