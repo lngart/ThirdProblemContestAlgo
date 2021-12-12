1ce191730b5b87ba74d2196fc975e206d646dcac
Artem <lngaart55@gmail.com>
2021-12-12 23:23:32 +0300

    Corrections

--- B_ComponentWeight/main.cpp
+++ B_ComponentWeight/main.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 #include <numeric>
 #include <vector>
+#include <functional>
 
-template <typename T>
+template <typename T, class Function>
 class DisjointSet {
 public:
     explicit DisjointSet(size_t num_sets);
 
-    void Union(T first, T second, size_t weight);
+    void Union(T first, T second, size_t weight = 1);
     size_t GetCurrentWeight(T element);
 
 private:
@@ -17,61 +18,63 @@ private:
 
     std::vector<T> parent_;
     std::vector<size_t> size_, weight_;
+
+    Function operation_;
 };
 
-template <typename T>
-DisjointSet<T>::DisjointSet(size_t num_sets) : parent_(num_sets), size_(num_sets), weight_(num_sets) {
-    std::iota(parent_.begin(), parent_.end(), 0);
+template <typename T, class Function>
+DisjointSet<T, Function>::DisjointSet(size_t num_sets) : parent_(num_sets), size_(num_sets), weight_(num_sets) {
+    for (size_t i{0}; i < num_sets; ++i) {
+        parent_[i] = i;
+    }
 }
 
-template <typename T>
-size_t DisjointSet<T>::Find(T element) {
+template <typename T, class Function>
+size_t DisjointSet<T, Function>::Find(T element) {
     if (element != parent_[element]) {
         parent_[element] = Find(parent_[element]);
     }
     return parent_[element];
 }
 
-template <typename T>
-void DisjointSet<T>::Union(T first, T second, size_t weight) {
+template <typename T, class Function>
+void DisjointSet<T, Function>::Union(T first, T second, size_t weight) {
     size_t first_id = Find(first), second_id = Find(second);
 
     if (first_id == second_id) {
-        weight_[first_id] += weight;
+        operation_(weight_[first_id], weight);
         return;
     }
 
-    if (size_[first_id] >= size_[second_id]) {
-        parent_[second_id] = first_id;
-        weight_[first_id] += (weight_[second_id] + weight);
-
-        if (size_[first_id] == size_[second_id]) {
-            ++size_[first_id];
-        }
-    } else {
-        parent_[first_id] = second_id;
-        weight_[second_id] += (weight_[first_id] + weight);
+    if (size_[first_id] < size_[second_id]) {
+        std::swap(first_id, second_id);
+    } else if (size_[first_id] == size_[second_id]) {
+        ++size_[first_id];
     }
+
+    parent_[second_id] = first_id;
+    auto new_weight = weight_[second_id] + weight;
+    operation_(weight_[first_id], new_weight);
 }
 
-template <typename T>
-size_t DisjointSet<T>::GetCurrentWeight(T element) {
+template <typename T, class Function>
+size_t DisjointSet<T, Function>::GetCurrentWeight(T element) {
     size_t element_id = Find(element);
     return weight_[element_id];
 }
 
 enum class TypeAction { ADD_EDGE = 1, GET_WEIGHT = 2 };
 
-template <typename T>
-void AddingEdge(DisjointSet<T> &connectivity_components) {
+template <typename T, class Function>
+void AddingEdge(DisjointSet<T, Function> &connectivity_components) {
     T start_vertex{0}, end_vertex{0};
     size_t weight_edge{0};
     std::cin >> start_vertex >> end_vertex >> weight_edge;
     connectivity_components.Union(start_vertex - 1, end_vertex - 1, weight_edge);
 }
 
-template <typename T>
-void FindWeightEdgesInComponent(DisjointSet<T> &connectivity_components, std::vector<T> &required_weights,
+template <typename T, class Function>
+void FindWeightEdgesInComponent(DisjointSet<T, Function> &connectivity_components, std::vector<T> &required_weights,
                                 size_t num_requests) {
     for (size_t i{0}; i < num_requests; ++i) {
         uint16_t type_action{0};
@@ -88,8 +91,8 @@ void FindWeightEdgesInComponent(DisjointSet<T> &connectivity_components, std::ve
     }
 }
 
-template <typename T>
-void PrintRequiredWeights(DisjointSet<T> &connectivity_components, size_t num_requests) {
+template <typename T, class Function>
+void PrintRequiredWeights(DisjointSet<T, Function> &connectivity_components, size_t num_requests) {
     std::vector<T> required_weights;
 
     FindWeightEdgesInComponent(connectivity_components, required_weights, num_requests);
@@ -99,6 +102,14 @@ void PrintRequiredWeights(DisjointSet<T> &connectivity_components, size_t num_re
     }
 }
 
+template <typename T>
+class Sum {
+public:
+    void operator()(T &lhs, T &rhs) {
+        lhs += rhs;
+    }
+};
+
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
@@ -107,7 +118,7 @@ int main() {
     size_t num_vertices{0}, num_requests{0};
     std::cin >> num_vertices >> num_requests;
 
-    DisjointSet<int> connectivity_components(num_vertices);
+    DisjointSet<size_t, Sum<size_t>> connectivity_components(num_vertices);
 
     PrintRequiredWeights(connectivity_components, num_requests);
 
